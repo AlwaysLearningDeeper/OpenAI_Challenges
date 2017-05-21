bcc2af3cbe55ad4aef2fd55ca2ceb14554f689c2
jaime <fhjaime96@gmail.com>
2017-05-21 18:24:42 +0200

    better ball capture, can't say the same for the platform

--- utils/screencp.py
+++ utils/screencp.py
@@ -1,6 +1,7 @@
 import numpy as np
-from Project.utils.grabscreen import grab_screen
-from Project.utils.directkeys import PressKey,ReleaseKey,A,W,S,D
+from utils.grabscreen import grab_screen
+from utils.directkeys import PressKey,ReleaseKey,A,W,S,D
+from matplotlib import pyplot as plt
 import cv2
 import time
 import sys
@@ -16,7 +17,7 @@ def draw_lines(img,lines):
 
 def draw_circles(img,circles):
     try:
-        for i in circles[0, :]:
+        for i in circles[0, :1]:
             # draw the outer circle
             cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
             # draw the center of the circle
@@ -63,22 +64,24 @@ def process_img(img):
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
     processed_img = cv2.GaussianBlur(processed_img, (3,3), 0 )
     vertices = np.array([[30, 265], [30, 145], [195, 145], [195, 265]])
-    processed_img = roi(processed_img, np.int32([vertices]))
+    #processed_img = roi(processed_img, np.int32([vertices]))
     verticesP = np.array([[30, 300], [30, 180], [197, 180], [197, 300]])
     platform = roi(processed_img, np.int32([verticesP]))
     #                       edges
-    #lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180,np.array([]), 120, 2)
+    #lines = cv2.HoughLinesP(platform, 1, np.pi/180, 180,np.array([]), 10, 2)
     #draw_lines(processed_img,lines)
+    #draw_lines(platform,lines)
+
+    #Platform lines
+    #imgray = cv2.cvtColor(platform,cv2.COLOR_BGR2GRAY)
+    ret,thresh = cv2.threshold(platform,127,255,0)
+    im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(original_image, contours, -1, (0,255,0), 3)
+
+
     circles = cv2.HoughCircles(processed_img, cv2.HOUGH_GRADIENT, 1, 20,
                                param1=90, param2=5, minRadius=1, maxRadius=3)
-    try:
-        for i in circles[0, :]:
-            # draw the outer circle
-            cv2.circle(original_image, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            # draw the center of the circle
-            cv2.circle(original_image, (i[0], i[1]), 2, (0, 0, 255), 3)
-    except:
-        pass
+    draw_circles(original_image,circles=circles)
 
     return processed_img,original_image,platform
 def main():
@@ -94,7 +97,7 @@ def main():
         if not paused:
             c+=1
             # 800x600 windowed mode
-            screen = grab_screen(title='FCEUX 2.2.3: Arkanoid (USA)')
+            screen = grab_screen(title='FCEUX 2.2.2: Arkanoid (USA)')
             if c%10==0:
                 print('Recording at ' + str((10 / (time.time() - last_time)))+' fps')
                 last_time = time.time()
@@ -103,9 +106,9 @@ def main():
             processed,original,platform=process_img(screen)
             #screen = cv2.resize(screen, (160,90))
 
-            cv2.imshow('window',platform)
-            cv2.imshow('window2', processed)
-            cv2.imshow('window3', original)
+
+
+            cv2.imshow('window',original)
             #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
