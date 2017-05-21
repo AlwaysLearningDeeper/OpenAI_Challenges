c0c6add5c53a1d7c634ed28eb55420173002fbd6
estrellita/jiwidi <fhjaime96@gmail.com>
2017-05-21 12:52:50 +0200

    arkanoid updates

--- utils/screencp.py
+++ utils/screencp.py
@@ -57,21 +57,30 @@ def roi(img,vertices):
     return masked
 
 
-def process_img(original_image):
-    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+def process_img(img):
+    original_image=img
+    processed_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
     processed_img = cv2.GaussianBlur(processed_img, (3,3), 0 )
-    vertices = np.array([[10,500],[10,300], [300,200], [500,200], [800,300], [800,500]], np.int32)
-    processed_img = roi(processed_img, [vertices])
-
+    vertices = np.array([[30, 265], [30, 145], [195, 145], [195, 265]])
+    processed_img = roi(processed_img, np.int32([vertices]))
+    verticesP = np.array([[30, 300], [30, 180], [197, 180], [197, 300]])
+    platform = roi(processed_img, np.int32([verticesP]))
     #                       edges
     #lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180,np.array([]), 120, 2)
     #draw_lines(processed_img,lines)
     circles = cv2.HoughCircles(processed_img, cv2.HOUGH_GRADIENT, 1, 20,
-                               param1=75, param2=10, minRadius=3, maxRadius=5)
-    draw_circles(processed_img,circles)
+                               param1=90, param2=5, minRadius=1, maxRadius=3)
+    try:
+        for i in circles[0, :]:
+            # draw the outer circle
+            cv2.circle(original_image, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            # draw the center of the circle
+            cv2.circle(original_image, (i[0], i[1]), 2, (0, 0, 255), 3)
+    except:
+        pass
 
-    return processed_img
+    return processed_img,original_image,platform
 def main():
 
     for i in list(range(4))[::-1]:
@@ -90,14 +99,13 @@ def main():
                 print('Recording at ' + str((10 / (time.time() - last_time)))+' fps')
                 last_time = time.time()
             screen = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
-            height, width, channels = screen.shape
-            vertices=np.array([[10,270],[10,100],[150,100],[150,270]])
-            print(height)
-            print(width)
-            screen=roi(screen,vertices)
-            screen=process_img(screen)
+
+            processed,original,platform=process_img(screen)
             #screen = cv2.resize(screen, (160,90))
-            cv2.imshow('window',screen)
+
+            cv2.imshow('window',platform)
+            cv2.imshow('window2', processed)
+            cv2.imshow('window3', original)
             #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
