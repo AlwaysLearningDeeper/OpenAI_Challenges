ab1c0db488b99e67affa01296147d4ecc9daa73b
estrellita/jiwidi <fhjaime96@gmail.com>
2017-05-21 09:43:47 +0200

    better fps counter, getting around 20 fps with the laptop

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
-from utils.directkeys import ReleaseKey, PressKey, W, A, S, D
-from utils.grabscreen import grab_screen
+from Project.utils.directkeys import ReleaseKey, PressKey, W, A, S, D
+from Project.utils.grabscreen import grab_screen
--- utils/screencp2.py
+++ utils/screencp2.py
@@ -1,5 +1,5 @@
 import numpy as np
-from utils.grabscreen import grab_screen
+from Project.utils.grabscreen import grab_screen
 import cv2
 import time
 import os
@@ -13,11 +13,16 @@ def main():
         time.sleep(1)
 
     paused = False
+    c=0
+    last_time = time.time()
     while(True):
         if not paused:
+            c+=1
             # 800x600 windowed mode
-            screen = grab_screen(title='FCEUX 2.2.2: Castlevania (USA)')
-            last_time = time.time()
+            screen = grab_screen(title='Registry editor')
+            if c%10==0:
+                print('Recording at ' + str((10 / (time.time() - last_time)))+' fps')
+                last_time = time.time()
             screen = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
             #screen = cv2.resize(screen, (160,90))
             cv2.imshow('window',screen)
