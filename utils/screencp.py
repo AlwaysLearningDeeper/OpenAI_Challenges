320eeb28e2c0a534ffd384719bbe35f7035116b2
estrellita/jiwidi <fhjaime96@gmail.com>
2017-05-22 11:30:32 +0200

    first IA version

--- utils/screencp.py
+++ utils/screencp.py
@@ -1,6 +1,6 @@
 import numpy as np
-from utils.grabscreen import grab_screen
-from utils.directkeys import PressKey,ReleaseKey,A,W,S,D
+from Project.utils.grabscreen import grab_screen
+from Project.utils.directkeys import PressKey,ReleaseKey,A,W,S,D
 from matplotlib import pyplot as plt
 import cv2
 import time
@@ -22,6 +22,7 @@ def draw_circles(img,circles):
             cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
             # draw the center of the circle
             cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
+            return [i[0],i[1]]
     except:
         pass
 def straight():
@@ -31,12 +32,14 @@ def straight():
 
 def left():
     PressKey(A)
+    time.sleep(0.1)
     ReleaseKey(W)
     ReleaseKey(D)
     ReleaseKey(A)
 
 def right():
     PressKey(D)
+    time.sleep(0.1)
     ReleaseKey(A)
     ReleaseKey(W)
     ReleaseKey(D)
@@ -63,10 +66,11 @@ def process_img(img):
     processed_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
     processed_img = cv2.GaussianBlur(processed_img, (3,3), 0 )
-    vertices = np.array([[30, 250], [30, 70], [195, 70], [195, 250]])
+    copy=processed_img
+    vertices = np.array([[30, 240], [30, 140], [195, 140], [195, 240]])
     processed_img = roi(processed_img, np.int32([vertices]))
-    verticesP = np.array([[30, 250], [30, 230], [197, 230], [197, 250]])
-    platform = roi(processed_img, np.int32([verticesP]))
+    verticesP = np.array([[30, 270], [30, 230], [197, 230], [197, 270]])
+    platform = roi(copy, np.int32([verticesP]))
     #                       edges
     #lines = cv2.HoughLinesP(platform, 1, np.pi/180, 180,np.array([]), 3, 2)
     #draw_lines(processed_img,lines)
@@ -77,13 +81,16 @@ def process_img(img):
     ret,thresh = cv2.threshold(platform,127,255,0)
     im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(original_image, contours, -1, (0,255,0), 3)
-
-
+    try:
+        platformpos=contours[0][0][0]
+    except:
+        platformpos=[[0]]
     circles = cv2.HoughCircles(processed_img, cv2.HOUGH_GRADIENT, 1, 20,
                                param1=90, param2=5, minRadius=1, maxRadius=3)
-    draw_circles(original_image,circles=circles)
 
-    return processed_img,original_image,platform
+    ballpos=draw_circles(original_image,circles=circles)
+
+    return processed_img,original_image,platform,platformpos,ballpos
 def main():
 
     for i in list(range(4))[::-1]:
@@ -93,20 +100,26 @@ def main():
     paused = False
     c=0
     last_time = time.time()
+
     while(True):
         if not paused:
-            c+=1
             # 800x600 windowed mode
-            screen = grab_screen(title='FCEUX 2.2.2: Arkanoid (USA)')
-            if c%10==0:
-                print('Recording at ' + str((10 / (time.time() - last_time)))+' fps')
-                last_time = time.time()
+            screen = grab_screen(title='FCEUX 2.2.3: Arkanoid (USA)')
+            #if c%10==0:
+            #   print('Recording at ' + str((10 / (time.time() - last_time)))+' fps')
+            #dd   last_time = time.time()
             screen = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
 
-            processed,original,platform=process_img(screen)
+            processed,original,platform,platformpos,ballpos=process_img(screen)
             #screen = cv2.resize(screen, (160,90))
 
-
+            try:
+                if (platformpos[0] - ballpos[0] > 0):
+                    left()
+                else:
+                    right()
+            except:
+                pass
 
             cv2.imshow('window',original)
             #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
