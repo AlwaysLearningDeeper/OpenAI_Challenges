039cd9778dda62bfc9eb15eab3b40d30916cbdb8
jaime <fhjaime96@gmail.com>
2017-05-22 15:45:41 +0200

    k

--- utils/screencp.py
+++ utils/screencp.py
@@ -1,6 +1,6 @@
 import numpy as np
-from Project.utils.grabscreen import grab_screen
-from Project.utils.directkeys import PressKey,ReleaseKey,A,W,S,D
+from utils.grabscreen import grab_screen
+from utils.directkeys import PressKey,ReleaseKey,A,W,S,D
 from matplotlib import pyplot as plt
 import cv2
 import time
@@ -32,16 +32,16 @@ def straight():
 
 def left():
     PressKey(A)
-    time.sleep(0.1)
     ReleaseKey(W)
     ReleaseKey(D)
+    time.sleep(0.1)
     ReleaseKey(A)
 
 def right():
     PressKey(D)
-    time.sleep(0.1)
     ReleaseKey(A)
     ReleaseKey(W)
+    time.sleep(0.1)
     ReleaseKey(D)
 
 def calmdown():
@@ -67,7 +67,7 @@ def process_img(img):
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
     processed_img = cv2.GaussianBlur(processed_img, (3,3), 0 )
     copy=processed_img
-    vertices = np.array([[30, 240], [30, 140], [195, 140], [195, 240]])
+    vertices = np.array([[30, 240], [30, 100], [195, 100], [195, 240]])
     processed_img = roi(processed_img, np.int32([vertices]))
     verticesP = np.array([[30, 270], [30, 230], [197, 230], [197, 270]])
     platform = roi(copy, np.int32([verticesP]))
@@ -91,6 +91,23 @@ def process_img(img):
     ballpos=draw_circles(original_image,circles=circles)
 
     return processed_img,original_image,platform,platformpos,ballpos
+
+def k(screen):
+        Z = screen.reshape((-1,3))
+
+        # convert to np.float32
+        Z = np.float32(Z)
+
+        # define criteria, number of clusters(K) and apply kmeans()
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        K = 2
+        ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+        # Now convert back into uint8, and make original image
+        center = np.uint8(center)
+        res = center[label.flatten()]
+        res2 = res.reshape((screen.shape))
+        return res2
 def main():
 
     for i in list(range(4))[::-1]:
@@ -100,28 +117,36 @@ def main():
     paused = False
     c=0
     last_time = time.time()
-
+    laspos=0
     while(True):
         if not paused:
             # 800x600 windowed mode
-            screen = grab_screen(title='FCEUX 2.2.3: Arkanoid (USA)')
-            #if c%10==0:
-            #   print('Recording at ' + str((10 / (time.time() - last_time)))+' fps')
-            #dd   last_time = time.time()
+            screen = grab_screen(title='FCEUX 2.2.2: Arkanoid (USA)')
+            if c%10==0:
+               print('Recording at ' + str((10 / (time.time() - last_time)))+' fps')
+            last_time = time.time()
             screen = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
+            screen=k(screen)
 
             processed,original,platform,platformpos,ballpos=process_img(screen)
-            #screen = cv2.resize(screen, (160,90))
+            screen = cv2.resize(screen, (160,90))
 
             try:
                 if (platformpos[0] - ballpos[0] > 0):
                     left()
+                    print('moving left')
                 else:
                     right()
+                    print('moving right')
             except:
                 pass
 
-            cv2.imshow('window',original)
+
+
+            cv2.imshow('window',processed)
+            cv2.imshow('window1',original)
+            cv2.imshow('window2',platform)
+
             #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
