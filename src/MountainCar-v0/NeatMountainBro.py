074de38897f55a0bf178999404de763416796551
Skymont <salvador19marti@gmail.com>
2017-08-07 18:40:02 +0200

    Improved Neat mountainCart-v0 to sometimes pass the gym challenge.

--- src/MountainCar-v0/NeatMountainBro.py
+++ src/MountainCar-v0/NeatMountainBro.py
@@ -4,9 +4,10 @@ import neat
 import os
 import random
 import time
-import multiprocessing
-
+from gym import wrappers
+p
 env = gym.make('MountainCar-v0')
+env = wrappers.Monitor(env,'tmp/MountainCar-v0')
 print("action space: {0!r}".format(env.action_space))
 print("observation space: {0!r}".format(env.observation_space))
 
@@ -19,8 +20,8 @@ max_reward = 200
 goal_steps = 200
 discounted_reward = 0.9
 score_range=[]
-trials=101
-generations=300
+trials=100
+generations=200
 #Fitness
 def compute_fitness(net, rewards, episodes):
     reward_error=[]
@@ -28,7 +29,6 @@ def compute_fitness(net, rewards, episodes):
     for reward, episode in zip(rewards,episodes):
         for(episode, reward) in zip(episode,[reward]):
             action=episode[2]
-
             output=net.activate(episode[1])
             reward_error.append((float(output[action]))**2)
     return reward_error
@@ -69,18 +69,19 @@ def eval_genomes(genomes, config):
     print(score_range)
     #Normalising
     normRewards=[]
+
     for i in range(len(episodes)):
         normRewards.append(2*(episodes[i][0]-min_reward)/(max_reward-episodes[i][0])-1.0)
 
-    comparison_episodes = [random.choice(episodes)[1] for _ in range(10)]
+    comparison_episodes = [random.choice(episodes)[1] for _ in range(100)]
     reward_errors=[]
-    """
+
     for genome,net in nets:
         reward_errors.append(compute_fitness(net,normRewards,comparison_episodes))
 
     for reward_error,(genome_id,genome) in zip(reward_errors,genomes):
-        genome.fitness= np.mean(reward_error)
-    """
+        genome.fitness -= np.mean(reward_error)
+
     print("Final fitness compute time {0}\n".format(time.time()-t0))
     #########################################################################
 
@@ -106,13 +107,11 @@ def run(config_file):
 
     # Show output of the most fit genome against training data.
     winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
-    observation=env.reset()
     scores=[]
     for i in range(trials):
         score=0
-        env.reset()
+        observation=env.reset()
         for _ in range(goal_steps):
-            env.render()
             action = np.argmax(winner_net.activate(observation))
             # do it!
             observation, reward, done, info = env.step(action)
@@ -120,6 +119,8 @@ def run(config_file):
             if done: break
         scores.append(score)
     print("The winning neural network obtained an average score of: "+str(np.average(scores)))
+    if(np.average(scores)>-110):
+        gym.upload('tmp/MountainCar-v0',api_key='sk_tiwKaUHVQDChjmO9JmK2Gg')
     p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
     p.run(eval_genomes, 10)
 
